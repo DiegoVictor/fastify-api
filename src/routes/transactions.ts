be1b07505f0fb353cf7a0966d771eb777250b1de
Diego Victor <diegovictorgonzaga@gmail.com>
2023-02-08 18:40:38 -0300

    Get transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,3 +1,22 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { knex } from '../database';
+
 export async function routes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+    return { transactions };
+  });
+
+  app.get('/:id', async request => {
+    const validator = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = validator.parse(request.params);
+
+    const transaction = await knex('transactions').where({ id }).first();
+
+    return { transaction };
+  });
+
 }
