332ca7c9a04a369b356d00f06ee524f9fecfa68a
Diego Victor <diegovictorgonzaga@gmail.com>
2023-02-09 11:15:29 -0300

    Register cookie handler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import cookie from '@fastify/cookie';
 import fastify from 'fastify';
 import { env } from './env';
 import { routes as transactions } from './routes/transactions';
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transactions, {
   prefix: 'transactions',
 });
