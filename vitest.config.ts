045f0a6b00714fdacb8f3afd4a089e9f10eecb40
Diego Victor <diegovictorgonzaga@gmail.com>
2023-02-11 12:19:37 -0300

    Configure vitest

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: 'tests/coverage',
+      exclude: ['db/**/*', 'src/database.ts', 'src/env.ts'],
+    },
+  },
+});
